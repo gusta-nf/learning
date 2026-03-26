39664a67744a964761a0f2da50489f427ead8cf8
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-03-25 21:05:43 -0300

    Segundo Desafio do Curso de Python, aprendendo primeiros comandos do Curso em Vídeo

---
+++ python/CursoemVídeo Python/Primeiros Comandos/Born.py
@@ -0,0 +1,4 @@
+day = input('Qual o dia em que você nasceu?')
+month = input('Em que mês você nasceu?')
+year = input('Em que ano você nasceu?')
+print('Você nasceu em',day,'de', month, 'no ano de', year,'. Correto?' )
