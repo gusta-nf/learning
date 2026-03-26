846169d91fc403d6912a901df64aeb35fcacac28
Gustavo Fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-03-25 22:02:58 -0300

    Explicação de como aprendi algo da solução
    Adicionado como aprendi a transformar o valor recebido no input para integers.

--- python/CursoemVídeo Python/Primeiros Comandos/plus2numbers.py
+++ python/CursoemVídeo Python/Primeiros Comandos/plus2numbers.py
@@ -1,3 +1,7 @@
+'''
+O professor Guanabar instruiu buscar na internet como transformar os valores recebidos no input como inteiro.
+Obtive a informação no site StackOverFlow o que me ajudou a resolver o Desafio
+'''
 number1 = int(input('Digite o primeiro número da soma '))
 number2 = int(input('Digite o segundo número da soma '))
 soma = number1 + number2
