82eedb95a966cc829c804e1c8e86390c5e769765
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-06-30 20:41:00 -0300

    4 Exercícios realizados do Livro Curso Intesivo de Python

---
+++ python/Curso Intensivo de Python/ex02_06name_cases.py
@@ -0,0 +1,3 @@
+famous_person = "Pythagoras"
+message = famous_person + " says: " + '"He who speaks, sows. He who listens, reaps."'
+print(message)
---
+++ python/Curso Intensivo de Python/ex02_09favorite_number.py
@@ -0,0 +1,2 @@
+my_favorite_number = 9
+print("In my childhood my favorite number was 7 but now is: " + str(my_favorite_number))
