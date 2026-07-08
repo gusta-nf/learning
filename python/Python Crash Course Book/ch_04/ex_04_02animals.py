b370aeba287cb0c5c008132831c81dc2a8d1d0c1
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-08 19:07:49 -0300

    Exercício de lista usando o laço for

---
+++ python/Python Crash Course Book/ch_04/ex_04_02animals.py
@@ -0,0 +1,7 @@
+animals = ['Whale', 'Jellyfish', 'Shark']
+for animal in animals:
+    print(animal)
+# Modifications
+for animal in animals:
+    print(animal + " is a good swimmer!")
+print("They are maritime animals!")
