738525ae8eb9b20d66c0b2037ea0e1413cbe8cc6
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-08 19:06:31 -0300

    Exercício de listas

---
+++ python/Python Crash Course Book/ch_04/ex_04_01pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ["Spicy sausage", "Portuguese-style", "Chicken w/ Brazilian cream cheese", "Chocolate"]
+for pizza in pizzas:
+    print("I like " + str(pizza) + " pizza!")
+print("I really like pizza!!")
