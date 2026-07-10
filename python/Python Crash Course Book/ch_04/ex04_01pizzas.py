eb7e0d0034983d38b05eac48cdca4c3d01456605
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-09 21:26:47 -0300

    Tirando uma função desnecessária do exercício

--- python/Python Crash Course Book/ch_04/ex04_01pizzas.py
+++ python/Python Crash Course Book/ch_04/ex04_01pizzas.py
@@ -1,4 +1,4 @@
 pizzas = ["Spicy sausage", "Portuguese-style", "Chicken w/ Brazilian cream cheese", "Chocolate"]
 for pizza in pizzas:
-    print("I like " + str(pizza) + " pizza!")
+    print("I like " + pizza + " pizza!")
 print("I really like pizza!!")
