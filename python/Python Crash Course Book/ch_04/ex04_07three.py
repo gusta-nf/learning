28d05f21b27f657c141a08b63cc56b1b8af4a357
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-13 18:04:13 -0300

    Mais 3 exercícios do livro

---
+++ python/Python Crash Course Book/ch_04/ex04_07three.py
@@ -0,0 +1,5 @@
+numbers = []
+for n30 in range(0,31,3):
+    numbers.append(n30)
+numbers.remove(0)
+print(numbers)
---
+++ python/Python Crash Course Book/ch_04/ex04_08cubes.py
@@ -0,0 +1,5 @@
+numbers = []
+for n in range(1,11):
+    n = n**3
+    numbers.append(n)
+print(numbers)
