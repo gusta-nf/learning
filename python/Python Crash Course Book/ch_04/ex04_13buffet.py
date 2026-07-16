a30280ccea9f0cee5617758d0d247afb06b11b6d
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-15 22:06:01 -0300

    Exercício de tuplas, verificando que não é possível alterar apenas sobreescrever.

---
+++ python/Python Crash Course Book/ch_04/ex04_13buffet.py
@@ -0,0 +1,12 @@
+buffet = ('Cold Cuts', 'Baked Salmon', 'Mashed Potatoes', 'Brownies', 'Fruit Juice')
+
+print('\tOld Menu:')
+for food in buffet:
+    print(food)
+
+# buffet.remove('Cold Cuts') - Proposital error
+
+print('\n\tNew Menu:')
+buffet = ('Cold Cuts', 'Baked Salmon', 'Mashed Potatoes', 'Mousse', 'Tea')
+for food in buffet:
+    print(food)
