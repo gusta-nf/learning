349b0f0a58633080c5b8fc7997a519ac3a646bfd
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-15 22:07:45 -0300

    Exercício pra testar como condicionais funcioname em Python

---
+++ python/Python Crash Course Book/ch_05/ex05_01conditional_tests.py
@@ -0,0 +1,27 @@
+red_fruit = 'apple'
+fruits = ['pineapple','mango','grape','strawberry']
+
+print("1-\tIs fruit == 'apple'? I predict True.") # 1 True
+print(red_fruit == 'apple')
+print("\n2-\tIs fruit == 'banana'? I predict False.") # 1 False
+print(red_fruit == 'banana')
+
+print("\n3-\tIs 'pineapple' in fruits list? I predict True.") # 2 True
+print('pineapple' in fruits)
+print("\n4-\tIs 'blueberry' in fruits list? I predict False.") # 2 False
+print('blueberry' in fruits)
+
+print("\n5-\t Is not 'kiwi' in fruits list? I predict True.") # 3 True
+print('kiwi' in fruits)
+print("\n6-\t Is not 'grape' in fruits list? I predict False.") # 3 False
+print('grape' in fruits)
+
+print("\n7-\tIs 'mango' != 'apple'? I predict True.") # 4 True
+print(red_fruit != 'mango')
+print("\n8-\tIs 'apple' != 'apple'? I predict False.") # 4 False
+print(red_fruit != 'apple')
+
+print("\n9-\tIs 'apple' not in fruit list? I predict True.") # 5 True
+print('apple' not in fruits)
+print("\n10-\tIs 'strawberry' not in fruit list? I predict False.") # 5 False
+print('strawberry' not in fruits)
