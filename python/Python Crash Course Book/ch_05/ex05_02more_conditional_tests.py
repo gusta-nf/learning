902d315ce4f5b063c9f484e04c6da9e997b7800f
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-15 22:08:54 -0300

    Exercício ampliando a quantidade de testes de condicionais do último exercício.

---
+++ python/Python Crash Course Book/ch_05/ex05_02more_conditional_tests.py
@@ -0,0 +1,70 @@
+red_fruit = 'apple'
+yellow_fruit = 'BANANA'
+fruits = ['pineapple','mango','grape','strawberry']
+
+print("1-\tIs fruit == 'apple'? I predict True.")
+print(red_fruit == 'apple')
+print("\n2-\tIs fruit == 'banana'? I predict False.")
+print(red_fruit == 'banana')
+
+print("\n3-\tIs 'pineapple' in fruits list? I predict True.")
+print('pineapple' in fruits)
+print("\n4-\tIs 'blueberry' in fruits list? I predict False.")
+print('blueberry' in fruits)
+
+print("\n5-\t Is not 'kiwi' in fruits list? I predict True.")
+print('kiwi' in fruits)
+print("\n6-\t Is not 'grape' in fruits list? I predict False.")
+print('grape' in fruits)
+
+print("\n7-\tIs 'mango' != 'apple'? I predict True.")
+print(red_fruit != 'mango')
+print("\n8-\tIs 'apple' != 'apple'? I predict False.")
+print(red_fruit != 'apple')
+
+print("\n9-\tIs 'apple' not in fruit list? I predict True.")
+print('apple' not in fruits)
+print("\n10-\tIs 'strawberry' not in fruit list? I predict False.")
+print('strawberry' not in fruits)
+
+print("\n11-\tIs 'Banana', and your variants, a yellow fruit? I predict True.")
+print(yellow_fruit.lower() == 'banana')
+
+print("\n12-\tIs 'water_melon', and your variants, a yellow fruit? I predict False.")
+print(yellow_fruit.lower() == 'water_melon')
+
+print("\n13-\tIs 4+4=8? I predict True.")
+print(4+4 == 8)
+
+print("\n14-\tIs 4+4=7? I predict False.")
+print(4+4 == 7)
+
+print("\n15-\tIs 4+4!=7? I predict True.")
+print(4+4 != 7)
+
+print("\n16-\tIs 4+4!=8? I predict False.")
+print(4+4 != 8)
+
+print("\n17-\tIs 4+4<=8? I predict True.")
+print(4+3 <= 8)
+
+print("\n18-\tIs 4+4>=9? I predict False.")
+print(4+3 >= 9)
+
+print("\n19-\tIs 4+5>7? I predict True.")
+print(4+5 > 7)
+
+print("\n20-\tIs 4+4<7? I predict False.")
+print(4+8 < 7)
+
+print("\n21-\tIs 10 == 10 and 9 == 9? I predict True.")
+print(10 == 10 and 9 == 9)
+
+print("\n22-\tIs 11 == 10 and 9 == 9? I predict False.")
+print(11 == 10 and 9 == 9)
+
+print("\n23-\tIs 7 == 7 or 6 == 9? I predict True.")
+print(7 == 7 or 6 == 9)
+
+print("\n24-\tIs 7 == 10 or 5 == 9? I predict False.")
+print(7 == 10 or 5 == 9)
