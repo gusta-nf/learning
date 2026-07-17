878f0d1bb825f32d7851f18715c4bd0f296482a0
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-16 21:40:05 -0300

    Exercício sobre condicionais no formato if-elif-else

---
+++ python/Python Crash Course Book/ch_05/ex05_05aliens_colors3.py
@@ -0,0 +1,24 @@
+# First
+alien_color = 'green'
+if alien_color.lower() == 'green':
+    print("Gain 5 points! - Green")
+elif alien_color.lower() == 'yellow':
+    print("Gain 10 points! - Yellow")
+elif alien_color.lower() == 'red':
+    print("Gain 15 points! - Red")
+# Second
+alien_color = 'yellow'
+if alien_color.lower() == 'green':
+    print("Gain 5 points! - Green")
+elif alien_color.lower() == 'yellow':
+    print("Gain 10 points! - Yellow")
+elif alien_color.lower() == 'red':
+    print("Gain 15 points! - Red")
+# Third
+alien_color = 'red'
+if alien_color.lower() == 'green':
+    print("Gain 5 points! - Green")
+elif alien_color.lower() == 'yellow':
+    print("Gain 10 points! - Yellow")
+elif alien_color.lower() == 'red':
+    print("Gain 15 points! - Red")
