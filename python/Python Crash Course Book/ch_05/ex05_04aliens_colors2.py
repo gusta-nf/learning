9ab3281f5584a0ef219a0c33d3083beb3de34b4b
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-16 21:38:48 -0300

    Exercício com condionais no formato if-else

---
+++ python/Python Crash Course Book/ch_05/ex05_04aliens_colors2.py
@@ -0,0 +1,12 @@
+# First
+alien_color = 'green'
+if alien_color.lower() == 'green':
+    print("Gain 5 points!")
+else:
+    print("Gain 10 points!")
+# Second
+alien_color = 'red'
+if alien_color.lower() == 'green':
+    print("Gain 5 points!")
+else:
+    print("Gain 10 points!")
