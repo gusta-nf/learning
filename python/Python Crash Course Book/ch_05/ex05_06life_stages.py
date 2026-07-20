76979a3d942f344a1f5428c500ac871fde587b6e
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-20 18:46:26 -0300

    Exercício sobre a estrutura de laço condicional if-elif-else

---
+++ python/Python Crash Course Book/ch_05/ex05_06life_stages.py
@@ -0,0 +1,16 @@
+# I could not use the operator 'and', but I even type lol
+age = 1
+if age < 2:
+    print("You are a baby!")
+elif age >= 2 and age < 4:
+    print("You are a child.")
+elif age >= 4 and age < 13:
+    print("You are so young!")
+elif age >= 13 and age < 20:
+    print("Your are  a tennager.")
+elif age >= 20 and age < 65:
+    print("Your are a adult.")
+elif age >= 65:
+    print("I'm a young adult. ;-)")
+else:
+    print("Or you have months of life or you don't exist.")
