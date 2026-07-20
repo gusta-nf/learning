585fecee9195ef61d6ea8e4c2c8fbe13f62f9b1c
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-20 18:49:38 -0300

    Exercício sobre o laço condicional if interagindo com lista

---
+++ python/Python Crash Course Book/ch_05/ex05_07favorite_fruit.py
@@ -0,0 +1,11 @@
+favorite_fruits = ['Strawberry', 'Mango', 'Kiwi']
+if 'Strawberry' in favorite_fruits:
+    print('You really like strawberries')
+if 'Mango' in favorite_fruits:
+    print('You like mango so much')
+if 'Kiwi' in favorite_fruits:
+    print('Kiwi is an excellent fruit!')
+if 'Apple' not in favorite_fruits:
+    print('Apple is good, but not WOW')
+if 'Banana' not in favorite_fruits:
+    print('Banana is ok')
