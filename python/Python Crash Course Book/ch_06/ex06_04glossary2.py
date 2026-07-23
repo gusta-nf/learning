a7648dae46d64016b833c767c504eaeb8ea0eb41
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-23 16:23:35 -0300

    Exercício de dicionário usando o laço for para maior eficiência

---
+++ python/Python Crash Course Book/ch_06/ex06_04glossary2.py
@@ -0,0 +1,13 @@
+glossary = {
+    'Traceback' : 'A explanation from errors',
+    'Conditionals' : 'Decisions based in True or False answers',
+    'Tuples' : "List which we can't altered",
+    'PEP 8' : "Official list of good conducts for programming",
+    'List Comprehension' : 'Lists compose for instructions, like a math function',
+    'List Indices' : 'Position of elements in a list',
+    'Conjuntion' : 'A list withou repeated elements',
+    'Float' : 'Numbers with decimal parts',
+    'Slices' : 'Parts of a list'
+}
+for word,meaning in glossary.items():
+    print('\n\tMeaning of ' + word + "\n" + meaning)
