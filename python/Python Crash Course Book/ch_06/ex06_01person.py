3cf2e9625dd6e53764c8c3821ceb9eb5780e0345
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-22 22:01:55 -0300

    Exercício de dicionários em python

---
+++ python/Python Crash Course Book/ch_06/ex06_01person.py
@@ -0,0 +1,10 @@
+person = {
+    'first_name' : 'Joao', 
+    'last_name' : 'Batista', 
+    'age' : 21, 
+    'city': 'Angra',
+}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
