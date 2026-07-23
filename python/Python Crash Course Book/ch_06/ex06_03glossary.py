5669ecdddbf3716f83980c2e6f6c9ce20c34e385
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-23 16:22:19 -0300

    Exercício de dicionário, o inglês pode estar errado, mas acredito que está funcional(prefiro ser sincero em dizer que estou aprendendo do que deixar tudo perfeito)

---
+++ python/Python Crash Course Book/ch_06/ex06_03glossary.py
@@ -0,0 +1,12 @@
+glossary = {
+    'traceback' : 'A explanation from errors',
+    'conditionals' : 'Decisions based in True or False answers',
+    'tuples' : "List which we can't altered",
+    'pep8' : "Official list of good conducts for programming",
+    'list_comprehension' : 'Lists compose for instructions, like a math function',
+}
+print('\tMeaning of Traceback:\n' + glossary['traceback'])
+print('\n\tMeaning of Conditionals:\n' + glossary['conditionals'])
+print('\n\tMeaning of Tuples:\n' + glossary['tuples'])
+print('\n\tMeaning of PEP 8:\n' + glossary['pep8'])
+print('\n\tMeaning of List Comprehsion:\n' + glossary['list_comprehension'])
