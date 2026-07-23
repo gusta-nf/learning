afd9cf9f2e81df7e729fdbfd5796ec3ca501d792
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-22 22:23:11 -0300

    Exercício de dicionário com números inteiros

---
+++ python/Python Crash Course Book/ch_06/ex06_02favorite_numbers.py
@@ -0,0 +1,12 @@
+people = {
+    'joao vitor' : 7,
+    'lucas' : 7,
+    'caetano' : 25,
+    'fernanda' : 3,
+    'gustavo' : 9,
+}
+print("Gustavo's favorite number is " + str(people['gustavo']))
+print("Fernanda's favorite number is " + str(people['fernanda']))
+print("Caetano's favorite number is " + str(people['caetano']))
+print("Lucas's favorite number is " + str(people['lucas']))
+print("Joao Vitor's favorite number is " + str(people['joao vitor']))
