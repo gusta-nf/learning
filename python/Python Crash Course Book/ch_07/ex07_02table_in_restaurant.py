2dec9e4b8f63ded9335e8e6f864f5d48b848efb2
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-07-30 18:36:15 -0300

    Exercício de input int

---
+++ python/Python Crash Course Book/ch_07/ex07_02table_in_restaurant.py
@@ -0,0 +1,6 @@
+people = input('How many people are coming for dinner? ')
+people = int(people)
+if people > 8:
+    print('You will need to wait a free table for dinner.')
+else:
+    print('You can take this table for dinner.')
