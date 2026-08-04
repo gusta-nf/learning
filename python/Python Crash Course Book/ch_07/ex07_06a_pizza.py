26e9ce25b599410e35aac3f23bb05dbba82343b2
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-04 20:58:13 -0300

    Exercício treinando encerrar laço com o break

---
+++ python/Python Crash Course Book/ch_07/ex07_06a_pizza.py
@@ -0,0 +1,9 @@
+while True:
+    ingredient = input("What ingredient do you add in your pizza?\n (Type 'quit' to exit the program)\n")
+    if ingredient != 'quit':
+        print("I'm going to add " + ingredient + " to your pizza.")
+    else:
+        break
+# Flag - done
+# Break - done
+# Conditional
