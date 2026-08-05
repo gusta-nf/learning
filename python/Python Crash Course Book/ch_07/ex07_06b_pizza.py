afb01e294e8e4d8e27ca2db3366f00d0fcb214cd
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-04 21:01:49 -0300

    Exercício encerrando laço utilizando condicional

---
+++ python/Python Crash Course Book/ch_07/ex07_06b_pizza.py
@@ -0,0 +1,9 @@
+ingredient = ''
+while ingredient != 'quit':
+    ingredient = input("What ingredient do you add in your pizza?\n (Type 'quit' to exit the program)\n")
+    print("I'm going to add " + ingredient + " to your pizza.")
+    if ingredient == 'quit':
+        print('')
+# Flag - done
+# Break - done
+# Conditional - done
