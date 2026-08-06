0d909626d147b0c79e46b239c4098fffc176c0df
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-06 20:05:53 -0300

    Melhorando a legebilidade

--- python/Python Crash Course Book/ch_07/ex07_08cafeteria.py
+++ python/Python Crash Course Book/ch_07/ex07_08cafeteria.py
@@ -1,9 +1,11 @@
 sandwich_orders = ['Salad Sandwich', 'Double Sandwich', 'Chesse Sandwich']
 finished_sandwiches = []
+
 while sandwich_orders: #When sandwich_orders is empty, it's same of False
     current_sandwich = sandwich_orders.pop()
     print("I'm preparing your " + current_sandwich)
     finished_sandwiches.append(current_sandwich)
+    
 print('\nThe following sandwiches have been finished:')
 for sandwich in finished_sandwiches:
     print(sandwich.title())
