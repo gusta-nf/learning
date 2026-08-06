199ac23bf03f2c1c0fe7d5af6c702dfe4bc2e602
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-05 22:34:46 -0300

    Exercício de laço while resolvido a partir do Flag

---
+++ python/Python Crash Course Book/ch_07/ex07_06a_cinema.py
@@ -0,0 +1,16 @@
+active = True
+while active:
+    age = input('\n\tCinema Ticket prices(0, Turn off)' + '\nHow old are you? ')
+    age = int(age)
+    if age == 0:
+        print('Shutdown.')
+        active = False
+    elif age < 3:
+        print('Ticket is free for you.')
+    elif 3 <= age <= 12:
+        print('Ticket is 10$ for you.')
+    elif age > 12:
+        print('Ticket is 15$ for you.')
+# Flag - Done
+# Break - Done
+# Conditional -
