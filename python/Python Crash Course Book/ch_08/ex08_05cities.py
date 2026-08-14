4969d9676888a70950f61422e74f743dead8cf68
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-14 13:43:40 -0300

    Exercício de função treinando chamadas de função

---
+++ python/Python Crash Course Book/ch_08/ex08_05cities.py
@@ -0,0 +1,5 @@
+def describe_city(city_name, country = "Brazil"):
+    print(city_name + " is localized in " + country + ".") #located
+describe_city(city_name = "Rio de Janeiro")
+describe_city("São Paulo")
+describe_city(city_name = "Madrid", country = "Spain")
