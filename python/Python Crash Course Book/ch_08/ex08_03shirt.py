731c5c81592d3c0d4f2a550c4de91dd7c409fecd
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-14 13:40:31 -0300

    Exercício sobre funções em python, treinando chamadas de função com argumentos posicionais e keyboard arguments

---
+++ python/Python Crash Course Book/ch_08/ex08_03shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size, text):
+    print(
+    "We'll produce a new shirt with " + size + " size" +
+    " and this text on " + text
+    )
+make_shirt("M", "You're cool.")
+make_shirt(size = "G", text = "Let's go to the gym.")
