7e8d604baf8f7d733c524a526169d2f8e4afe8e0
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-14 13:42:21 -0300

    Exercício de função em python, treinando chamadas com valor default, argumentos posicionais e argumentos nomeados

---
+++ python/Python Crash Course Book/ch_08/ex08_04big_shirts.py
@@ -0,0 +1,8 @@
+def make_shirt(size = "G", text = "I love Python"):
+    print(
+    "We'll produce a new shirt with " + size + " size" +
+    " and this text on " + text
+    )
+make_shirt()
+make_shirt("M")
+make_shirt(size = "P", text = "We are Python fans!")
