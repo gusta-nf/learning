9f21561fdea1b812b2203b24ade940e42ee65267
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-18 19:10:54 -0300

    Exercício de função com dicionário adicionando um looping e entrada do usuário

---
+++ python/Python Crash Course Book/ch_08/ex08_08user_albums.py
@@ -0,0 +1,37 @@
+def make_album(album_artist, album_title, album_tracks = ''):
+    album = {
+        'artist' : album_artist,
+        'title' : album_title
+        }
+    if album_tracks:
+        album['tracks'] = album_tracks
+    return album
+
+while True:
+
+    artist_var = input(
+        "What's the name of your favorite artist?" + 
+        "(Type 'q' and Enter for exit)\n"
+    )
+    if artist_var == 'q':
+        break
+
+
+    title_var = input(
+        "What is the name of the famous album from your favorite artist?" + 
+        "(Type 'q' and Enter for exit)\n"
+    )
+    if title_var == 'q':
+        break
+
+
+    tracks_var = input(
+        "How many tracks does this album have?(Press enter to skip)" +
+        "(Type 'q' and Enter for exit)\n"
+        )
+    if tracks_var == 'q':
+        break
+    
+# Printing, but not saving.
+    album_var = make_album(artist_var, title_var, tracks_var)
+    print(album_var)
