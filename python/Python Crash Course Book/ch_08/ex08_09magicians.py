bdbaa284acd8cf40ff5ea6bb195f5b8f4772b089
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-19 21:07:35 -0300

    Exercício de função em python interagindo com uma lista

---
+++ python/Python Crash Course Book/ch_08/ex08_09magicians.py
@@ -0,0 +1,7 @@
+magicians_names = ['Mister M', 'Patrick Jane', 'Harry Houdini']
+
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician)
+
+show_magicians(magicians = magicians_names)
