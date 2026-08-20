b58d68d93f7ba427a268e795e4a804e450db1317
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-08-19 21:09:21 -0300

    Exercício de Python modificando uma lista usando funções(admito ter demorado incialmente)

---
+++ python/Python Crash Course Book/ch_08/ex08_10great_magicians.py
@@ -0,0 +1,12 @@
+magicians_names = ['Mister M', 'Patrick Jane', 'Harry Houdini']
+
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians):
+    for position in range(0, len(magicians)): # len for any list size
+        magicians[position] = 'The Great ' + magicians[position]
+
+make_great(magicians = magicians_names)
+show_magicians(magicians = magicians_names)
