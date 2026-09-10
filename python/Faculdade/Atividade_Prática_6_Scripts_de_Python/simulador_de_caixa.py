9c71219d11a4ca89c4bf98b721a3ed6931abb9d6
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-09-10 19:40:30 -0300

    Comentários para atualizar código depois

--- python/Faculdade/Atividade_Prática_6_Scripts_de_Python/simulador_de_caixa.py
+++ python/Faculdade/Atividade_Prática_6_Scripts_de_Python/simulador_de_caixa.py
@@ -6,15 +6,19 @@ while True:
     notas = [100, 50, 20, 10, 5, 2]
 
     if (val % 5 != 0) and (val % 2 != 0):
+        ''' Melhor colocar esse IF no final e lembrar que o 7 por mais que não
+        seja divísivel ele também entra por ser 5+2'''
         print("Não é possível gerar esse troco, "
         "o valor não é divísivel pelas notas disponíveis.")
         break
 
     for nota in notas:
+        ''' Pensar no IF para finais pares maiores que 5, para dividir por 2
+        Finais ímpares para tirar 5 e depois complentar com 2'''
         a = val // nota
         val = val - (a * nota)
         trocados.append(a)
-
+# Gostei da solução abaixo para mostrar nota e trocados
     for nota in notas:
         b = "Seu troco em notas de R$" + str(nota) + " é: "
         print(b + str(trocados[0]))
